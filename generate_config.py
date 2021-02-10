bb43a6a29f25ce2d4f6dda3b90144d988f9154b9
Steve Crouch <s.crouch@software.ac.uk>
2021-02-10 12:26:54 +0000

    Update to Jan 2021

--- generate_config.py
+++ generate_config.py
@@ -6,7 +6,7 @@ LOGFILE_DIR = 'logs'
 # Start and end dates you want for stats
 # (Defaults indicate dates for which GA data is currently available)
 STARTDATE = '2010-04-01'
-ENDDATE = '2020-12-31'
+ENDDATE = '2021-01-31'
 
 # The overall Google Analytics metrics you want
 # You can find the full list at
@@ -18,6 +18,8 @@ GA_OUTPUT_DIR = 'ga_raw_data'
 
 # Our URL list file to process
 URL_LIST_FILE = 'url_lists/training_guidestoptips.txt'
+#URL_LIST_FILE = 'url_lists/comms_blogposts.txt'
+#URL_LIST_FILE = 'url_lists/combined_blogpostsguidestoptips.txt'
 
 # Where to put output reports
 REP_OUTPUT_DIR = 'reports'
