ed4bac58e302057e3792735824ae67b614231ac6
Steve Crouch <s.crouch@software.ac.uk>
2021-02-11 08:52:57 +0000

    Update to Jan 2021

--- download_config.py
+++ download_config.py
@@ -9,7 +9,7 @@ LOGFILE_DIR = 'logs'
 # Start and end dates you want for stats
 # (Defaults indicate dates for which GA data is currently available)
 STARTDATE = '2010-04-01'
-ENDDATE = '2020-12-31'
+ENDDATE = '2021-01-31'
 
 # The overall Google Analytics metrics you want
 # You can find the full list at
