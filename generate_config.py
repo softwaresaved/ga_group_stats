b9322c75387ad3b3ac41aacd4b57e6c977f3b006
Steve Crouch <s.crouch@software.ac.uk>
2021-05-28 09:05:54 +0100

    Update raw data to end-April

--- generate_config.py
+++ generate_config.py
@@ -6,7 +6,7 @@ LOGFILE_DIR = 'logs'
 # Start and end dates you want for stats
 # (Defaults indicate dates for which GA data is currently available)
 STARTDATE = '2010-04-01'
-ENDDATE = '2021-01-31'
+ENDDATE = '2021-04-30'
 
 # The overall Google Analytics metrics you want
 # You can find the full list at
